f3ed281818941abf562e3f943cb9b4173e29dcee
Abiodun Osinaike <abiodu.osinaike@longbridgetech.com>
2021-01-22 10:27:41 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './Profile';
+import axios from 'axios';
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [profiles, setProfile] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      setLoading(true);
+      const response = await axios.get('https://api.enye.tech/v1/challenge/records');
+      let profileList = []
+      response.data.records.profiles.map(element => profileList.push(Object.values(element)));
+      setProfile(profileList);
+      setLoading(false);
+    };
+    fetchProfiles();
+ }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      loading ? <h3 style={{textAlign:"center"}}>Profiles loading...</h3> : <Profile profiles={profiles}/>
+    }
     </div>
   );
 }
